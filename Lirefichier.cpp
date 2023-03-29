1aef03dd0d955e3e346901179911108239f043e8
MaLi-iLaM <61450907+MaLi-iLaM@users.noreply.github.com>
2023-03-29 17:27:56 +0200

    Debug
    J'ai réduit l'écart d'un, mais j'ai besoin de conseil !

--- Lirefichier.cpp
+++ Lirefichier.cpp
@@ -3,6 +3,7 @@
 #include<fstream>
 #include<vector>
 #include<string>
+#include<sstream>
 
 std :: vector<std :: vector<std ::vector<int>>> lirefichier ()
 {
@@ -15,81 +16,92 @@ std :: vector<std :: vector<std ::vector<int>>> lirefichier ()
 
     std :: string ligne; 
     while(getline(monfichier, ligne)) 
+    {
+        std :: cout << compteur<<std :: endl;   
+        if (ligne[0] != '#') 
         {
-            
-            if (ligne[0] != '#') 
+            if (compteur == 0)
             {
-                if (compteur == 0)
-                {
-                    std :: cout << "Ligne 1 ok" << std ::endl ;
-                }
-                else
-                {if (compteur == 1)
+                std :: string qualite ;
+                qualite = ligne;
+
+                std :: cout << "Ligne 1 ok " << qualite << std ::endl ;
+            }
+            else
+            {if (compteur == 1)
+                {                     
+                    std::string parseNum;
+                    std::stringstream streamLigne(ligne);
+                    int res=0;
+                    while(std::getline(streamLigne,parseNum,' '))
                     {
-                        int position = monfichier.tellg();
-                        std :: cout<< ligne << std :: endl ;
-                    
-                        monfichier >> resolution[0];
-                        std :: cout<< resolution[0]<< std ::endl;
-                        monfichier >> resolution[1] ;
-                        std :: cout<< resolution[1]<< std ::endl;
+                        resolution[res] = std::stoi(parseNum);
+                        res++;
+                    } 
 
 
-                        std :: cout << "Ligne 2 ok" << std ::endl ;
+                    std :: cout << "Ligne 2 ok" << std ::endl ;
 
-                    }
-                else
-                {if (compteur==2)
-                {
-                    
-                    std :: cout << "Ligne 3 ok" << std ::endl ;
                 }
-                else 
+            else
+            {if (compteur==2)
+            {
+                int valmax = std :: stoi(ligne);
+                std :: cout << "Ligne 3 ok " << valmax << std ::endl ;
+            }
+            else 
+            {if(compteur == 3)
                 {
                     std :: vector<int>  vecteur  (3,0);
                     std :: vector<std :: vector<int> > intermediaire (resolution[1], vecteur)  ;
                     std :: vector<std :: vector< std:: vector<int>>> image (resolution[0],intermediaire) ;
                     
                     
-                                   
+                                
                     for (int i = 0;i<resolution[0];i++)
                     { 
-                        std :: cout<< "ligne "<< i<< std ::endl;
+                        //std :: cout<< "ligne "<< i<< std ::endl;
                         for (int j =0; j< resolution[1]; j++)
                         {
-                            std :: cout<< "colonne "<< j<< std ::endl;
+                            //std :: cout<< "colonne "<< j<< std ::endl;
 
                             for (int k=0; k<3;k++) 
                             {
-                            std :: cout<< "couleur "<< k<< std ::endl;
-                            getline(monfichier,ligne);
+                            //std :: cout<< "couleur "<< k<< std ::endl;
                             monfichier >> image[i][j][k] ;
+                            getline(monfichier,ligne);
+
+                            
+                            
                                 
                             }
                         }
+
                     }
                     std :: cout<< image[0][0][0] << " 0,1,1"<< std :: endl ;
-
-                    std :: cout<< image[0][0][1] << " 1,1,1"<< std :: endl ;
-                    std :: cout << image[0][0][2] << " 2,2,0"<< std :: endl;
+                    
+                    std :: cout<< image[29][29][1] << " 1,1,1"<< std :: endl ;
+                    std :: cout << image[29][29][2] << " 2,2,0"<< std :: endl;
 
                     return image;
                 
                 }
+                else
+                {
+                    std :: vector<int>  vecteur  (0,0);
+                    std :: vector<std :: vector<int> > intermediaire (0, vecteur)  ;
+                    std :: vector<std :: vector< std:: vector<int>>> image (0,intermediaire) ;
+                    return image ; 
                 }
             }
-            compteur ++;
-
             }
+        }
+        compteur ++;
 
         }
-   }
-    std :: vector<int>  vecteur  (0,0);
-    std :: vector<std :: vector<int> > intermediaire (0, vecteur)  ;
-    std :: vector<std :: vector< std:: vector<int>>> image (0,intermediaire) ;
-   return image ;
-   
 
+    }
+   }
 }
 
 
