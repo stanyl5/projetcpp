2633d6abca803f04e36c5be36ce22b1a64b92de9
Tristan Lomré <103626726+stanyl5@users.noreply.github.com>
2023-04-04 10:53:52 +0200

    Update code_tristan.cpp

--- code_tristan.cpp
+++ code_tristan.cpp
@@ -120,12 +120,14 @@ void ecrire_fichier(Image A){
             }
             
         }
+
+        fichier.close();
 }
     
 
-    fichier.close();
+    
 
-}
+
 
 struct Matrice
 {
@@ -137,7 +139,7 @@ struct Matrice
   {
     ligne = lig;
     colonne = col;
-    mat = std::vector<std::vector<int>>(ligne, std::vector<int>(colonne, 0));
+    mat = std::vector<std::vector<int>>(ligne, std::vector<int>(colonne, 255));
   }
 
   void AfficheMatrice()
@@ -146,7 +148,7 @@ struct Matrice
     {
       for (int y = 0; y < colonne; ++y)
       {
-        std::cout<<mat[x][y]<< " ";
+        std::cout <<mat[x][y]<< " ";
       }
 
       std::cout << std::endl;
@@ -166,13 +168,11 @@ Matrice dessiner_ligne(Matrice mat, float m, float p){
     if (abs(m)<=1){
             for(int j = 0; j < colonnes; j++) {
                 if (((int)(m*j+p)<=lignes-1)&&((int)(m*j+p)>=0)){ // pour eviter que la droite "depasse" l'image
-                    //std::cout<< (int)(m*j+p) << " "<< m*j+p<< " " << Norme((int)(m*j+p),m*j+p)*255<< " ";
-                    //std::cout << int(m*j+p)/(m*j+p)<< " "; 
-                    
-                    mat.mat[(int)(m*j+p)][j] = Norme((int)(m*j+p),m*j+p)*255;                    
+                                       
+                    mat.mat[(int)(m*j+p)][j] = 255-(1-Norme((int)(m*j+p),m*j+p))*255;                    
                 }
                 if (((int)(m*j+p)+1<=lignes-1)&&((int)(m*j+p)+1>=0)){ // pour eviter que la droite "depasse" l'image
-                    mat.mat[(int)(m*j+p)+1][j] = (1-Norme((int)(m*j+p),m*j+p))*255;                    
+                    mat.mat[(int)(m*j+p)+1][j] = 255-Norme((int)(m*j+p),m*j+p)*255;                    
                 }
             }
     }
@@ -181,10 +181,10 @@ Matrice dessiner_ligne(Matrice mat, float m, float p){
             if (((int)((i-p)/m)<=colonnes-1)&&((int)((i-p)/m)>=0)){ // pour eviter que la droite "depasse" l'image
                 //std::cout<< (int)((i-p)/m) << " "<< ((i-p)/m)<< " " << Norme((int)((i-p)/m),((i-p)/m))*255<< " "<< (1-Norme((int)((i-p)/m),((i-p)/m)))*255<< " ";
 
-                mat.mat[i][(int)((i-p)/m)] = Norme((int)((i-p)/m),((i-p)/m))*255;
+                mat.mat[i][(int)((i-p)/m)] = 255-(1-Norme((int)((i-p)/m),((i-p)/m)))*255;
             }
             if (((int)((i-p)/m)+1<=colonnes-1)&&((int)((i-p)/m)+1>=0)){ // pour eviter que la droite "depasse" l'image
-                    mat.mat[i][(int)((i-p)/m)+1] = (1-Norme((int)((i-p)/m),((i-p)/m)))*255;                    
+                    mat.mat[i][(int)((i-p)/m)+1] = 255-Norme((int)((i-p)/m),((i-p)/m))*255;                    
                 }
         }
     }
@@ -199,7 +199,7 @@ int main ()
 
     ecrire_fichier(A);
     Matrice mat2(A.ligne, A.colonne);
-    mat2 = dessiner_ligne(mat2, -0.25, 3);
+    mat2 = dessiner_ligne(mat2, 1.8, 3); 
     std::cout<<"\n";
     std::cout<<"\n";
     mat2.AfficheMatrice();
@@ -209,7 +209,7 @@ int main ()
     int colonnes = mat2.colonne;
         for(int i = 0; i < lignes; i++){
             for(int j = 0; j < colonnes; j++){
-                if (mat2.mat[i][j]!=0){
+                if (mat2.mat[i][j]!=255){
                     A.image[i][j][0]=mat2.mat[i][j];
                     A.image[i][j][1]=mat2.mat[i][j];
                     A.image[i][j][2]=mat2.mat[i][j];
